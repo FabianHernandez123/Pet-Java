a6ab3db8f6877aff6f090ba991cbbd4823001a76
Fabian Hernandez <h.fabian25f@gmail.com>
2021-02-07 02:34:20 -0600

    Added a main method to test out classes

--- pet/Main.java
+++ pet/Main.java
@@ -7,8 +7,13 @@ public class Main {
 		
 		//Book class
 		Book newLibro = new Book(750, "paperback", "autofiction");
-		System.out.println(newLibro.bookSize());
-		System.out.println(newLibro.isTextbook());
+		System.out.println("Book size: " + newLibro.bookSize());
+		System.out.println("Is this a textbook? " + newLibro.isTextbook());
+		
+		//Pet class
+		Pet bunnicula = new Pet("bunnicula", 7, "Iceland", "Rabbit");
+		bunnicula.setName("Cloudy2.0");
+		System.out.printf("Rabbit name: %s", bunnicula.getName());
 	}
 
 }
